09f750fcbdf2b43180abbbfe0f9549c83fbfaf01
fvfvgb <1225490599@qq.com>
2021-10-06 19:51:11 +0800

    Import ElementPlus

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <el-container class="forum-container">
+        <el-aside>
+            <el-menu router>
+                <el-menu-item index="/">
+                    <i class="el-icon-user-solid"></i>
+                    <template #title>Home</template>
+                </el-menu-item>
+                <el-menu-item index="/about">
+                    <i class="el-icon-user-solid"></i>
+                    <template #title>About</template>
+                </el-menu-item>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-scrollbar style="flex-grow: 1;">
+                <el-header>
+                    <el-avatar>USER</el-avatar>
+                </el-header>
+                <el-main>
+                    <router-view />
+                </el-main>
+            </el-scrollbar>
+        </el-container>
+    </el-container>
 </template>
 
 <style lang="scss">
+html, body, #app, .forum-container {
+  width: 100%;
+  height: 100%;
+}
+
+.forum-container {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
