7fd9ec5d4dcbc519470f5187da2c6189ff2f9deb
fvfvgb <1225490599@qq.com>
2021-10-23 22:51:48 +0800

    修复CDN引入错误

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,28 @@
 const cdnSettings = {
-    dist: {
+    production: {
         externals: {
             'vue': 'Vue',
             'vuex': 'Vuex',
             'vue-router': 'VueRouter',
-            'element-plus': 'ELEMENT',
+            'element-plus': 'ElementPlus',
             'axios': 'axios',
         },
         css: [
+            'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css',
             'https://cdn.jsdelivr.net/npm/element-plus@1.1.0-beta.24/dist/index.css',
-            'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css',
         ],
         js: [
+            'https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.global.prod.js',
+            'https://cdn.jsdelivr.net/npm/vuex@4.0.2/dist/vuex.global.prod.js',
+            'https://cdn.jsdelivr.net/npm/vue-router@4.0.11/dist/vue-router.global.prod.js',
             'https://cdn.jsdelivr.net/npm/element-plus@1.1.0-beta.24/dist/index.full.min.js',
-            'https://cdn.jsdelivr.net/npm/vue@3.2.19/dist/vue.global.min.js',
-            'https://cdn.jsdelivr.net/npm/vuex@4.0.2/dist/vuex.global.min.js',
-            'https://cdn.jsdelivr.net/npm/vue-router@4.0.11/dist/vue-router.global.min.js',
-            'https://cdn.jsdelivr.net/npm/axios@0.23.0/dist/axios.min.js'
+            'https://cdn.jsdelivr.net/npm/axios@0.23.0/dist/axios.min.js',
         ]
+    },
+    development: {
+        externals: {},
+        css: [],
+        js: []
     }
 }
 
@@ -26,16 +31,18 @@ module.exports = {
     chainWebpack: config => {
         config.plugin('html').tap(args => {
             if (process.env.NODE_ENV === 'production') {
-                args[0].cdn = cdnSettings.dist
+                args[0].cdn = cdnSettings.production
+            } else {
+                args[0].cdn = cdnSettings.development
             }
             return args
         })
     },
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-            config.externals = cdnSettings.dist.externals
+            config.externals = cdnSettings.production.externals
         } else {
-            config.externals = {}
+            config.externals = cdnSettings.development.externals
         }
     }
 }
